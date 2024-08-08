53233f1500a349ab7a88282045ee0821e259f130
Arthur Bréant <arthur@thinkr.fr>
2024-08-08 17:31:39 +0200

    chore: recreate signature app with R

---
+++ dev/01_start.R
@@ -0,0 +1,63 @@
+# Building a Prod-Ready, Robust Shiny Application.
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+#
+#
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+##
+## /!\ Note: if you want to change the name of your app during development,
+## either re-run this function, call golem::set_golem_name(), or don't forget
+## to change the name in the app_sys() function in app_config.R /!\
+##
+golem::fill_desc(
+  pkg_name = "signature.r",
+  pkg_title = "Fill your email signature",
+  pkg_description = "Fill in your email signature from Thinkr template.",
+  author_first_name = "Arthur",
+  author_last_name = "Bréant",
+  author_email = "arthur@thinkr.fr",
+  repo_url = "https://github.com/ThinkR-open/signature.r"
+)
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Install the required dev dependencies ----
+golem::install_dev_deps()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license()
+usethis::use_readme_rmd(open = FALSE)
+devtools::build_readme()
+# Note that `contact` is required since usethis version 2.1.5
+# If your {usethis} version is older, you can remove that param
+usethis::use_code_of_conduct(contact = "arthur@thinkr.fr")
+usethis::use_lifecycle_badge("Experimental")
+usethis::use_news_md(open = FALSE)
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+
+## Add helper functions ----
+golem::use_utils_ui(with_test = TRUE)
+golem::use_utils_server(with_test = TRUE)
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile("dev/02_dev.R")
