1b19272b591982dfcd0ebd3d2892f85400510311
Arthur Bréant <arthur@thinkr.fr>
2025-01-15 11:35:07 +0100

    fix: update navbar brand name from 'Signature.r' to 'Signature.yo'

--- R/mod_navbar.R
+++ R/mod_navbar.R
@@ -18,7 +18,7 @@ mod_navbar_ui <- function(id) {
         a(
           class = "navbar-brand",
           href = "/",
-          "Signature.r"
+          "Signature.yo"
         ),
         input_dark_mode()
       )
